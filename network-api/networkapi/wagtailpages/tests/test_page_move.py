cfdd02d3128fcafa5d8b3cbed57f3c7f39b66bb5
Kevin <kbhowbrook@gmail.com>
2023-01-30 09:13:17 +0000

    test more locales

--- network-api/networkapi/wagtailpages/tests/test_page_move.py
+++ network-api/networkapi/wagtailpages/tests/test_page_move.py
@@ -3,11 +3,11 @@ import http
 from django.contrib.auth.models import User
 from django.urls import reverse
 from wagtail.contrib.redirects.models import Redirect
+from wagtail.core.models import Locale
 
 from networkapi.wagtailpages.factory import buyersguide as buyersguide_factories
 from networkapi.wagtailpages.factory import publication as publication_factory
 from networkapi.wagtailpages.tests import base as test_base
-from wagtail.core.models import Locale
 
 
 class PageRedirectTest(test_base.WagtailpagesTestCase):
@@ -26,16 +26,24 @@ class PageRedirectTest(test_base.WagtailpagesTestCase):
         self.article_page_under_index_page_old_path = self.article_page_under_index_page.url
 
         # Translate some pages
-        self.fr_homepage = self.homepage.copy_for_translation(self.fr_locale)
-
         self.en_locale = Locale.objects.get(language_code="en")
         self.fr_locale = Locale.objects.get(language_code="fr")
+        self.de_locale = Locale.objects.get(language_code="de")
+
+        self.fr_homepage = self.homepage.copy_for_translation(self.fr_locale)
+        self.de_homepage = self.homepage.copy_for_translation(self.de_locale)
 
         self.fr_article_page_under_home_page = self.article_page_under_home_page.copy_for_translation(self.fr_locale)
         self.fr_article_index_page = self.article_index_page.copy_for_translation(self.fr_locale)
         self.fr_article_page_under_index_page = self.article_page_under_index_page.copy_for_translation(self.fr_locale)
 
         self.fr_article_page_under_index_page_old_path = self.fr_article_page_under_index_page.url
+
+        self.de_article_page_under_home_page = self.article_page_under_home_page.copy_for_translation(self.de_locale)
+        self.de_article_index_page = self.article_index_page.copy_for_translation(self.de_locale)
+        self.de_article_page_under_index_page = self.article_page_under_index_page.copy_for_translation(self.de_locale)
+
+        self.de_article_page_under_index_page_old_path = self.de_article_page_under_index_page.url
 
     def test_page_loads(self):
         response = self.client.get(path=self.article_page_under_home_page.get_url())
@@ -58,12 +66,14 @@ class PageRedirectTest(test_base.WagtailpagesTestCase):
         self.assertEqual(redirect.old_path + "/", self.article_page_under_index_page_old_path)
         self.assertEqual(redirect.redirect_page_id, self.article_page_under_index_page.id)
 
-    def test_page_move_creates_redirect_french(self):
+    def test_page_move_creates_redirect_fr(self):
         # Check there are no redirects yet
         self.assertFalse(Redirect.objects.all())
         # Move the article under the index page out under the home page.
         response = self.client.post(
-            reverse("wagtailadmin_pages:move_confirm", args=(self.fr_article_page_under_index_page.id, self.fr_homepage.id)),
+            reverse(
+                "wagtailadmin_pages:move_confirm", args=(self.fr_article_page_under_index_page.id, self.fr_homepage.id)
+            ),
             follow=True,
         )
         # Check the 'Page moved' message to show it's been moved
@@ -74,3 +84,22 @@ class PageRedirectTest(test_base.WagtailpagesTestCase):
         # +'/' because old path doesn't store trailing slash
         self.assertEqual(redirect.old_path + "/", self.fr_article_page_under_index_page_old_path)
         self.assertEqual(redirect.redirect_page_id, self.fr_article_page_under_index_page.id)
+
+    def test_page_move_creates_redirect_de(self):
+        # Check there are no redirects yet
+        self.assertFalse(Redirect.objects.all())
+        # Move the article under the index page out under the home page.
+        response = self.client.post(
+            reverse(
+                "wagtailadmin_pages:move_confirm", args=(self.de_article_page_under_index_page.id, self.de_homepage.id)
+            ),
+            follow=True,
+        )
+        # Check the 'Page moved' message to show it's been moved
+        self.assertContains(response, f"Page &#x27;{self.de_article_page_under_index_page}&#x27; moved")
+        self.assertTrue(Redirect.objects.all())
+        redirect = Redirect.objects.first()
+        # check the redirect
+        # +'/' because old path doesn't store trailing slash
+        self.assertEqual(redirect.old_path + "/", self.de_article_page_under_index_page_old_path)
+        self.assertEqual(redirect.redirect_page_id, self.de_article_page_under_index_page.id)
